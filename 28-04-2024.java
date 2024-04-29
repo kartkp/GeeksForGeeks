8044ba00b69045ebc27a831f49cd2d5729995140
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-04-29 21:32:18 +0530

    Update 28-04-2024.java

--- 28-04-2024.java
+++ 28-04-2024.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    Node deleteMid(Node head) {
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        if (count <= 1)
+            return null;
+        temp = head;
+        int mid = count / 2;
+        while (mid - 1 > 0) {
+            temp = temp.next;
+            mid--;
+        }
+        temp.next = temp.next.next;
+        return head;
+    }
+}
