cb8f306f45de78e4c4743121d53c0389bf9226cd
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-05-17 20:48:25 +0530

    Create 17-05-2024.java

---
+++ 17-05-2024.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPair(int n, int x, int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < n; ++i) {
+            if (seen.contains(arr[i] - x) || seen.contains(arr[i] + x)) {
+                return 1;
+            }
+            seen.add(arr[i]);
+        }
+
+        return -1;
+    }
+}
