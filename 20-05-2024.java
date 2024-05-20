f40d879b02f04a751f6d61ade74438f628b15132
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-05-20 11:37:33 +0530

    Create 20-05-2024.java

---
+++ 20-05-2024.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long PowMod(long x, long n, long m) {
+        if (n == 0) return 1;
+
+        x %= m;
+        long half = PowMod(x, n / 2, m);
+
+        if (n % 2 == 0) {
+            return (half * half) % m;
+        } else {
+            return (((half * half) % m) * x) % m;
+        }
+    }
+}
